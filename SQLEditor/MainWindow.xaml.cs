040bb9a7fe790262dcad69e35ba1ce4e6b3af44d
Miroslav Jandak <mjandak@csc.com>
2018-09-06 23:49:32 +0200

    add database tree view with context menus add top menu - open, create db

--- SQLEditor/MainWindow.xaml.cs
+++ SQLEditor/MainWindow.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using AurelienRibon.Ui.SyntaxHighlightBox;
+using Microsoft.Win32;
+using Common;
+using System.Data.Common;
+using System.Configuration;
+using System.IO;
+using System.Data.SQLite;
 
 namespace SQLEditor
 {
@@ -27,26 +33,46 @@ namespace SQLEditor
             sqlEditor.CurrentHighlighter = HighlighterManager.Instance.Highlighters["SQL"];
         }
 
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			((MainWindowVM)((FrameworkElement)sender).DataContext).Run();
-		}
-
         private void lblTable_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             ((TableVM)((FrameworkElement)sender).DataContext).ShowContents();
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void btnNewTable_Click(object sender, RoutedEventArgs e)
         {
-            NewTableWindow x = new NewTableWindow();
+            var x = new NewTableWindow();
             x.ShowDialog();
         }
-	}
+
+        void btnOpebDb_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = sender as Button;
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
+
+        void menuOpenDb_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ((MainWindowVM)DataContext).AddDatabase(System.IO.Path.GetFileName(openFileDialog.FileName), openFileDialog.FileName);
+                ((MainWindowVM)DataContext).LoadDatabase(System.IO.Path.GetFileName(openFileDialog.FileName));
+            }
+        }
+
+        private void menuCreateDb_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                File.Create(dialog.FileName).Dispose();
+                ((MainWindowVM)DataContext).AddDatabase(System.IO.Path.GetFileName(dialog.FileName), dialog.FileName);
+                ((MainWindowVM)DataContext).LoadDatabase(System.IO.Path.GetFileName(dialog.FileName));
+            }
+        }
+    }
 }
--- SQLEditor/MainWindowVM.cs
+++ SQLEditor/MainWindowVM.cs
@@ -8,94 +8,109 @@ using System.Data.SQLite;
 using System.Configuration;
 using System.Diagnostics;
 using Common;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Data.Common;
 
 namespace SQLEditor
 {
-    public class MainWindowVM : INotifyPropertyChanged
+    public class MainWindowVM : ViewModelBase
     {
-        private string _sql;
+        public class ExecuteCommand : ICommand
+        {
+            private MainWindowVM _parent;
+
+            public event EventHandler CanExecuteChanged;
+
+            public ExecuteCommand(MainWindowVM parent)
+            {
+                _parent = parent;
+                _parent.PropertyChanged += _parent_PropertyChanged;
+            }
+
+            private void _parent_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _parent.DbVM != null && !string.IsNullOrWhiteSpace(_parent.Sql);
+            }
+
+            public void Execute(object parameter)
+            {
+                _parent.Run();
+            }
+        }
+
+        public ExecuteCommand ExecuteCmd { get; set; }
+
+        string _sql;
         public string Sql
         {
             get { return _sql; }
             set
             {
                 _sql = value;
-                if (this.PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("Sql"));
-                }
+                Notify(nameof(Sql));
             }
         }
 
-        private DataTable _queryResult = null;
+        private DataTable _queryResult;
         public DataTable QueryResult
         {
             get { return _queryResult; }
             set
             {
                 _queryResult = value;
-                if (this.PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("QueryResult"));
-                }
+                Notify(nameof(QueryResult));
             }
         }
 
-        private double _lastSqlDuration;
+        double _lastSqlDuration;
         public double LastSqlDuration
         {
             get { return _lastSqlDuration; }
             set
             {
                 _lastSqlDuration = value;
-                if (this.PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("LastSqlDuration"));
-                }
+                Notify(nameof(LastSqlDuration));
             }
         }
 
-        private List<TableVM> _tables = new List<TableVM>();
-        public List<TableVM> Tables
+        public DbVM DbVM
         {
-            get { return _tables; }
+            get => _dbVM;
             set
             {
-                _tables = value;
-                if (this.PropertyChanged != null)
-                {
-                    PropertyChanged(this, new PropertyChangedEventArgs("Tables"));
-                }
+                _dbVM = value;
+                Notify(nameof(DbVM));
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<DbConnVM> ConnStrings { get; set; } = new ObservableCollection<DbConnVM>();
+
+        private TableVM _selectedTable;
+        private DbVM _dbVM;
+
+        public TableVM SelectedTable
+        {
+            get => _selectedTable;
+            set => _selectedTable = value;
+        }
+
+        public ICommand RefreshCmd { get; set; }
 
         public MainWindowVM()
         {
-            var cmd = new SQLiteCommand(@"
-                SELECT name FROM sqlite_master
-                WHERE type='table'
-                ORDER BY name;",
-            Globals.DbConnection);
-            cmd.Connection.OpenEx();
-            SQLiteDataReader r = cmd.ExecuteReader();
-            while (r.Read())
+            ExecuteCmd = new ExecuteCommand(this);
+            RefreshCmd = new RelayCommand<string>(LoadDatabase);
+
+            foreach (ConnectionStringSettings item in ConfigurationManager.ConnectionStrings)
             {
-                Tables.Add(new TableVM(r[0].ToString(), this));
+                ConnStrings.Add(new DbConnVM(this) { Name = item.Name, ConnString = item.ConnectionString });
             }
-
-            foreach (var t in Tables)
-            {
-                var cmd2 = new SQLiteCommand($"pragma table_info({t.Name});", Globals.DbConnection);
-                cmd2.ExecuteReader();
-            }
-
-            cmd.Connection.Close();
-
-
-            
-            
         }
 
         public void Run()
@@ -116,22 +131,150 @@ namespace SQLEditor
             {
                 QueryResult = ds.Tables[0];
             }
+        }
 
+        public void LoadDatabase(string dbName)
+        {
+            dbName = dbName ?? throw new ArgumentException(nameof(dbName));
 
+            Globals.DbConnection?.Dispose();
+            Globals.DbConnection = new SQLiteConnection(ConfigurationManager.ConnectionStrings[dbName].ConnectionString);
+
+            var cmd = new SQLiteCommand(@"
+                SELECT name FROM sqlite_master
+                WHERE type='table'
+                ORDER BY name;",
+            Globals.DbConnection);
+
+            //using (Globals.DbConnection)
+            //{
+            cmd.Connection.OpenEx();
+            SQLiteDataReader r = cmd.ExecuteReader();
+
+            var dbVM = new DbVM(this) { Name = dbName };
+
+            QueryResult = null;
+
+            while (r.Read())
+            {
+                var table = new TableVM(r[0].ToString(), this);
+                dbVM.Tables.Add(table);
+
+                var cmd2 = new SQLiteCommand($"pragma table_info({r[0].ToString()});", Globals.DbConnection);
+                var r2 = cmd2.ExecuteReader();
+                while (r2.Read())
+                {
+                    DataTypes type = DataTypes.text;
+                    Enum.TryParse(r2["type"].ToString(), out type);
+                    table.Columns.Add(new ColumnVM(r2["name"].ToString(), type));
+                }
+            }
+            DbVM = dbVM;
+            //}
+        }
+
+        public void AddDatabase(string name, string filePath)
+        {
+            var csb = new DbConnectionStringBuilder
+                {
+                    { "data source", filePath }
+                };
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var css = new ConnectionStringSettings(name, csb.ConnectionString);
+            if (config.ConnectionStrings.ConnectionStrings.IndexOf(css) > 0)
+                config.ConnectionStrings.ConnectionStrings.Remove(css);
+            config.ConnectionStrings.ConnectionStrings.Add(css);
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            ConnStrings.Add(new DbConnVM(this) { Name = css.Name, ConnString = css.ConnectionString });
+        }
+    }
+
+    public class DbVM
+    {
+        MainWindowVM _parent;
+        public string Name { get; set; }
+        public ObservableCollection<TableVM> Tables { get; set; } = new ObservableCollection<TableVM>();
+        public DbVM(MainWindowVM parent)
+        {
+            _parent = parent;
         }
     }
 
     public class TableVM
+
     {
+        public class GenerateSqlCmd : ICommand
+        {
+            TableVM _parent;
+            public event EventHandler CanExecuteChanged;
+
+            public GenerateSqlCmd(TableVM parent)
+            {
+                _parent = parent;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                var sql = "";
+                switch (parameter.ToString())
+                {
+                    case "select":
+                        sql = $"select {string.Join(",", _parent.Columns.Select(c => c.Name))} from {_parent.Name}";
+                        break;
+                    case "update":
+                        sql = $"update {_parent.Name} set";
+                        break;
+                    case "insert":
+                        sql = $"insert into {_parent.Name} ({string.Join(",", _parent.Columns.Select(c => c.Name))}) values ()";
+                        break;
+                    case "delete":
+                        sql = $"delete from {_parent.Name}";
+                        break;
+                    case "drop":
+                        sql = $"drop table {_parent.Name}";
+                        break;
+                    default:
+                        break;
+                }
+                _parent.MainWindowVM.Sql += $"{Environment.NewLine}{sql}";
+            }
+
+        }
+
+        public GenerateSqlCmd GenerateSql { get; set; }
+
         public MainWindowVM MainWindowVM { get; set; }
+
         public string Name { get; set; }
 
-        public IEnumerable<ColumnVM> Columns { get; set; }
+        public List<ColumnVM> Columns { get; set; }
 
         public TableVM(string tblName, MainWindowVM mainWinVM)
         {
             MainWindowVM = mainWinVM;
             Name = tblName;
+            GenerateSql = new GenerateSqlCmd(this);
+            Columns = new List<ColumnVM>();
+            //var columns = new List<ColumnVM>();
+            //using (Globals.DbConnection)
+            //{
+            //    var cmd2 = new SQLiteCommand($"pragma table_info({Name});", Globals.DbConnection);
+            //    var r = cmd2.ExecuteReader();
+            //    while (r.Read())
+            //    {
+            //        DataTypes type = DataTypes.text;
+            //        Enum.TryParse<DataTypes>(r["type"].ToString(), out type);
+            //        columns.Add(new ColumnVM(r["name"].ToString(), type));
+            //    }
+            //}
+            //Columns = columns;
         }
 
         public void ShowContents()
@@ -147,16 +290,44 @@ namespace SQLEditor
             MainWindowVM.LastSqlDuration = sw.Elapsed.TotalMilliseconds;
             MainWindowVM.QueryResult = dt;
         }
-
-
     }
 
-    //public class Column
-    //{
-    //    public string Name { get; set; }
-    //    public Column()
-    //    {
+    public class DbConnVM
+    {
+        public MainWindowVM MainWindowVM { get; set; }
+        public ICommand LoadDbCmd { get; set; }
+        public string Name { get; set; }
+        public string ConnString { get; set; }
+        public DbConnVM(MainWindowVM mainWinVM)
+        {
+            MainWindowVM = mainWinVM;
+            LoadDbCmd = new LoadDbCmd(this);
+        }
+        public void LoadDatabase()
+        {
+            MainWindowVM.LoadDatabase(Name);
+        }
+    }
 
-    //    }
-    //}
+    public class LoadDbCmd : ICommand
+    {
+        DbConnVM _owner;
+
+        public event EventHandler CanExecuteChanged;
+
+        public LoadDbCmd(DbConnVM owner)
+        {
+            _owner = owner;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _owner.LoadDatabase();
+        }
+    }
 }
